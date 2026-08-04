d24c09c154afec9a359ee85d4d18d09adda454d9
Caleb Finney <calebfinney45@gmail.com>
2026-08-04 07:04:36 +0300

    Scaffold Flask app with JWT, Bcrypt, and RESTful API setup

---
+++ server/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_restful import Api
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
+from models import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'change-this-in-a-real-app'
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+api = Api(app)
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
